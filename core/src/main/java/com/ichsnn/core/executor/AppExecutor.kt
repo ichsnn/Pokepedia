a1ef3f5ea79cb4f9c91d00a351905a02f559fffa
ichsnn <ichsan.nulmuhlis@gmail.com>
2023-10-14 11:10:05 +0700

    chore: remove unused

--- core/src/main/java/com/ichsnn/core/executor/AppExecutor.kt
+++ core/src/main/java/com/ichsnn/core/executor/AppExecutor.kt
@@ -1,37 +1,17 @@
 package com.ichsnn.core.executor
 
-import android.os.Handler
-import android.os.Looper
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 import javax.inject.Inject
 
 class AppExecutor constructor(
     private val diskIO: Executor,
-    private val networkIO: Executor,
-    private val mainThread: Executor
 ) {
     @Inject
     constructor() : this(
         Executors.newSingleThreadExecutor(),
-        Executors.newFixedThreadPool(THREAD_COUNT),
-        MainThreadExecutor()
     )
 
     fun diskIO(): Executor = diskIO
-
-    fun networkIO(): Executor = networkIO
-
-    private class MainThreadExecutor : Executor {
-        private val mainThreadHandler = Handler(Looper.getMainLooper())
-
-        override fun execute(command: Runnable) {
-            mainThreadHandler.post(command)
-        }
-    }
-
-    companion object {
-        private const val THREAD_COUNT = 3
-    }
 
 }
